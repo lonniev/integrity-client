fe747e81409ce248813c2c8c20438d86f5c841d9
Lonnie VanZandt <lonniev@gmail.com>
2014-11-25 10:59:07 -0700

    get the right direction for the link resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -68,9 +68,11 @@ directory "#{installDir}" do
   action :create
 end
 
-link "/lib/x86_64-linux-gnu/libc.so.6" do
-  to "/lib64/libc.so.6"
+link "/lib64/libc.so.6" do
+  to "/lib/x86_64-linux-gnu/libc.so.6"
   only_if "test -f /lib/x86_64-linux-gnu/libc.so.6"
+  not_if "test -f /lib/libc.so.6"
+  not_if "test -f /lib64/libc.so.6"
 end
 
 execute "silently install the client" do
